ec12f42e1362a25c4adfc05713c8f88749d0ab58
itrustu0 <cornell.andrew09@gmail.com>
2020-05-14 14:14:36 -0700

    Deleted cmake stuffs

--- BuildingEscape/cmake-build-debug/CMakeFiles/BuildingEscapeEditor-Win64-DebugGame.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/BuildingEscapeEditor-Win64-DebugGame"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BuildingEscapeEditor-Win64-DebugGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
